90528bce72b5518df3f5a658e6f2e24192c5eaf3
zbieracz2000 <zbieracz2000@interia.pl>
2023-06-18 11:55:06 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_11.0"
-export DT_LINK="https://github.com/zbieracz2000/android_sony_kitakami_twrp.git -b sumire"
+export FOX_BRANCH="fox_7.1"
+export DT_LINK="https://github.com/ACherepkov1989/android_sony_kitakami_twrp.git -b sumire"
 
 export DEVICE="sumire"
 export OEM="sony"
